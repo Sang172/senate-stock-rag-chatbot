693ad2b6dad8359e7463ba1d814f57fd411de028
Sang172 <sang.ahn.94@gmail.com>
2025-02-02 01:29:56 -0800

    update yml

--- Dockerfile
+++ Dockerfile
@@ -24,4 +24,4 @@ COPY . .
 EXPOSE 5000
 
 # Command to run your application
-CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -169,7 +169,5 @@ def chat():
     response = rag.run(user_input)
     return jsonify({"response": response, "history": rag.memory}) 
 
-"""
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
-"""
