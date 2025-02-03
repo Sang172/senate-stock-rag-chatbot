ff5845b7fbb072fdd9cee6c2934e5542524c5e73
Sang172 <sang.ahn.94@gmail.com>
2025-02-02 17:29:45 -0800

    update data pipeline

--- app.py
+++ app.py
@@ -164,6 +164,6 @@ def chat():
     return jsonify({"response": response, "history": rag.memory}) 
 
 if __name__ == "__main__":
-    logging.info("About to start Flask app on port 8080")
+    logging.info("About to start Flask app on port 5000")
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
